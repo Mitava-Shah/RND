69da612fd09fa78ed6924dd04155ec51f3f36b35
Mitava-Shah <mitvashah792@gmail.com>
2024-11-27 13:15:43 +0530

    commit

--- RND/vite.config.js
+++ RND/vite.config.js
@@ -3,14 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    mimeTypes: {
-      "application/javascript": ["js"],
-    },
-    proxy: {
-      "/api": {
-        target: "http://localhost:3006", 
-     },
-    },
-  },
+  // server: {
+  //   mimeTypes: {
+  //     "application/javascript": ["js"],
+  //   },
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:3006", 
+  //    },
+  //   },
+  // },
 });
